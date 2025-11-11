d720b848c69914343ed3db06bc189ce79e029aee
Klyde Sangalang <sangalangklyde@gmail.com>
2025-11-11 18:47:30 +0800

    Fixed

--- src/main/java/com/parkingreserve/project1/model/UserApp.java
+++ src/main/java/com/parkingreserve/project1/model/UserApp.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class AppUser {
+public class UserApp {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
